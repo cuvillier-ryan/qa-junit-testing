16c34022cbc18433947b0366627756cfa9875e76
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2021-05-26 09:09:43 -0500

    Scanner-Calculator class update

--- src/main/java/com/qa/junitexample/Main.java
+++ src/main/java/com/qa/junitexample/Main.java
@@ -8,40 +8,40 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Choose a subject:\n - add\n - subtract\n - multiply\n - division");
+		System.out.println("Choose a subject:\n - add\n - subtract\n - multiply\n - division\n");
 		String result = scanner.nextLine();
 
 		switch (result) {
 		case "add":
-			System.out.println("Please provide the first number:");
+			System.out.println("Please provide the first number:\n ");
 			int add1 = Integer.parseInt(scanner.nextLine());
-			System.out.println("Please provide the second number:");
+			System.out.println("Please provide the second number:\n");
 			int add2 = Integer.parseInt(scanner.nextLine());
-			System.out.println(add1 + " + " + add2 + " = " + Calculator.add(add1, add2));
+			System.out.println("\n" + add1 + " + " + add2 + " = " + Calculator.add(add1, add2));
 			break;
 
 		case "subtract":
-			System.out.println("Please provid the first number:");
+			System.out.println("Please provid the first number:\n");
 			int sub1 = Integer.parseInt(scanner.nextLine());
-			System.out.println("Please provide the second number: ");
+			System.out.println("Please provide the second number:\n");
 			int sub2 = Integer.parseInt(scanner.nextLine());
-			System.out.println(sub1 + " - " + sub2 + " = " + Calculator.subtract(sub1, sub2));
+			System.out.println("\n" + sub1 + " - " + sub2 + " = " + Calculator.subtract(sub1, sub2));
 			break;
 
 		case "multiply":
-			System.out.println("Please provid the first number:");
+			System.out.println("Please provid the first number:\n");
 			int mult1 = Integer.parseInt(scanner.nextLine());
-			System.out.println("Please provide the second number:");
+			System.out.println("Please provide the second number:\n");
 			int mult2 = Integer.parseInt(scanner.nextLine());
-			System.out.println(mult1 + " * " + mult2 + " = " + Calculator.multiply(mult1, mult2));
+			System.out.println("\n" + mult1 + " * " + mult2 + " = " + Calculator.multiply(mult1, mult2));
 			break;
 
 		case "division":
-			System.out.println("Please provid the first number:");
+			System.out.println("Please provid the first number:\n");
 			int div1 = Integer.parseInt(scanner.nextLine());
-			System.out.println("Please provide the second number:");
+			System.out.println("Please provide the second number:\n");
 			int div2 = Integer.parseInt(scanner.nextLine());
-			System.out.println(div1 + " / " + div2 + " = " + Calculator.divide(div1, div2));
+			System.out.println("\n" + div1 + " / " + div2 + " = " + Calculator.divide(div1, div2));
 			break;
 
 		default:
@@ -50,7 +50,7 @@ public class Main {
 
 		}
 
-		System.out.println("Exitiing app");
+		System.out.println("Thank you, Exiting app.");
 		scanner.close();
 
 	}
